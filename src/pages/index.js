42b051caf4d115cea15023f0f6badcad153fec8d
markusmatilainen <markus.matilainen@edu.savonia.fi>
2020-08-31 13:47:39 +0300

    Fixes to verification

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Index = () => {
+    return (
+        <div>
+            <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+            <h1>Welcome</h1>
+        </div>
+      )
+}
+
+export default Index
